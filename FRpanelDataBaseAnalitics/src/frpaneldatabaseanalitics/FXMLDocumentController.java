f398f734c7f84e1578ecda510262d5d12c64822f
RasulaMakbul <emu.cfs@gmail.com>
2020-12-11 12:50:20 +0600

    login and import page

---
+++ FRpanelDataBaseAnalitics/src/frpaneldatabaseanalitics/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frpaneldatabaseanalitics;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private Button LoginBtn;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void LoginBtnAction(ActionEvent event) throws IOException {
+        Parent ImportsceneParent = FXMLLoader.load(getClass().getResource("/importPage/fileImport.fxml"));
+        Scene ImportScene= new Scene(ImportsceneParent);
+        
+        Stage stage= new Stage();
+        stage.setScene(ImportScene);
+        stage.show();
+    }
+    
+}
